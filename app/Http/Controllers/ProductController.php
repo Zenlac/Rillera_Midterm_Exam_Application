e2651e75bd4fabf44f8113a8e764fd2ca564a6d8
Zenlac <caleleizengrillera@gmail.com>
2024-02-21 12:43:12 +0800

    Finalized display of products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+
+        $ProductData = array(
+            "Title"=> "Jujutsu Kaisen","Attack on Titan", "Your lie in April",
+            "Classroom of the Elite", "Solo Leveling", "Demon Slayer", "Sword Art Online"
+        );
+        
+        return view('Product',['ProductData' => $ProductData]);
+    }
+
 }
---
+++ resources/views/Product.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Anime</title>
+</head>
+<body>
+    <div>
+        <h1>Anime</h1>
+    </div>
+
+    <div>
+        {{$ProductData}}
+    </div>
+</body>
+</html>
